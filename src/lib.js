af7893ec30f4f17f63ab631b898d8932f255789f
Léo Torrez <leo@leotorrez.com.br>
2017-10-20 15:25:53 -0300

    TDD

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,4 @@
-const FizzBuzz = (num) =>{
+const FizzBuzz = (num) => {
   if (num === 0) return 0;
   if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
   if (num % 3 === 0) return 'Fizz';
@@ -6,9 +6,5 @@ const FizzBuzz = (num) =>{
 
   return num;
 };
-
-function foo(){
-  return 'bla';
-}
 
 export default FizzBuzz;
